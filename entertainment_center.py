8a68757f4737054a2e5a9929fa26a933befb9ec1
jenningchen <jenningnchen@gmail.com>
2017-06-08 16:32:01 -0400

    added a few comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,50 @@
-Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
+import fresh_tomatoes
+import media
+
+#multiple instances of the Movie class
+inception = media.Movie("Inception",
+                        "A man who plants ideas in the subconscious",
+                        "http://www.impawards.com/2010/posters/inception_xlg.jpg",
+                        "https://www.youtube.com/watch?v=66TuSJo4dZM")
+
+zootopia = media.Movie("Zootopia",
+                       "A planet of animals with a mystery to be solved",
+                       "http://www.impawards.com/2016/posters/zootopia_xlg.jpg",
+                       "https://www.youtube.com/watch?v=jWM0ct-OLsM")
+la_la_land = media.Movie("La La Land",
+                         "A musical romance between two aspiring entertainers",
+                         "https://images-na.ssl-images-amazon.com/images/I/61pVLV%2Bz11L._AC_UL320_SR216,320_.jpg",
+                         "https://www.youtube.com/watch?v=0pdqf4P9MB8")
+
+whiplash = media.Movie("Whiplash",
+                       "A young drummer enters conflict with his instructor",
+                       "http://i.wp.pl/a/f/film/008/64/81/0408164.jpg",
+                       "https://www.youtube.com/watch?v=7d_jQycdQGo")
+
+wonder_woman = media.Movie("Wonder Woman",
+                           "A sheltered Amazon princess leaves her home"
+                           "to end a raging war",
+                           "http://cdn1-www.comingsoon.net/assets/uploads/gallery/wonder-woman/wwpost345.jpg",
+                           "https://www.youtube.com/watch?v=1Q8fG0TtVAY")
+
+sing_street = media.Movie("Sing Street",
+                          "A high-schooler in Dublin forms a band to"
+                          "impress a girl he likes",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzODA3MDcxMl5BMl5BanBnXkFtZTgwODgxNDk3NzE@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+                          "https://www.youtube.com/watch?v=C_YqJ_aimkM")
+                           
+                       
+#list of Movie instances
+movies = [inception,zootopia,la_la_land,whiplash,wonder_woman,sing_street]
+
+#open webpage
+fresh_tomatoes.open_movies_page(movies)
+
+
+
+
+#print(media.Movie.VALID_RATINGS )
+
+#print(media.Movie.__doc__)
+#print(media.Movie.__name__)
+#print(media.Movie.__module__)
