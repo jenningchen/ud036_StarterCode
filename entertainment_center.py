b835a1d4a1f5939544fa674ccc39b9167dafbf00
jenningchen <jenningnchen@gmail.com>
2017-06-15 22:17:34 -0400

    Improving code stylistic issues

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,20 @@
 import fresh_tomatoes
 import media
 
-#multiple instances of the Movie class
+# multiple instances of the Movie class
 inception = media.Movie("Inception",
                         "A man who plants ideas in the subconscious",
-                        "http://www.impawards.com/2010/posters/inception_xlg.jpg",
+                        "http://www.impawards.com/2010/posters/inception_xlg.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
 zootopia = media.Movie("Zootopia",
                        "A planet of animals with a mystery to be solved",
-                       "http://www.impawards.com/2016/posters/zootopia_xlg.jpg",
+                       "http://www.impawards.com/2016/posters/zootopia_xlg.jpg",  # NOQA
                        "https://www.youtube.com/watch?v=jWM0ct-OLsM")
+
 la_la_land = media.Movie("La La Land",
                          "A musical romance between two aspiring entertainers",
-                         "https://images-na.ssl-images-amazon.com/images/I/61pVLV%2Bz11L._AC_UL320_SR216,320_.jpg",
+                         "https://images-na.ssl-images-amazon.com/images/I/61pVLV%2Bz11L._AC_UL320_SR216,320_.jpg",  # NOQA
                          "https://www.youtube.com/watch?v=0pdqf4P9MB8")
 
 whiplash = media.Movie("Whiplash",
@@ -24,27 +25,19 @@ whiplash = media.Movie("Whiplash",
 wonder_woman = media.Movie("Wonder Woman",
                            "A sheltered Amazon princess leaves her home"
                            "to end a raging war",
-                           "http://cdn1-www.comingsoon.net/assets/uploads/gallery/wonder-woman/wwpost345.jpg",
+                           "http://cdn1-www.comingsoon.net/assets/uploads/gallery/wonder-woman/wwpost345.jpg",  # NOQA
                            "https://www.youtube.com/watch?v=1Q8fG0TtVAY")
 
 sing_street = media.Movie("Sing Street",
                           "A high-schooler in Dublin forms a band to"
                           "impress a girl he likes",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzODA3MDcxMl5BMl5BanBnXkFtZTgwODgxNDk3NzE@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzODA3MDcxMl5BMl5BanBnXkFtZTgwODgxNDk3NzE@._V1_UY1200_CR90,0,630,1200_AL_.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=C_YqJ_aimkM")
-                           
-                       
-#list of Movie instances
-movies = [inception,zootopia,la_la_land,whiplash,wonder_woman,sing_street]
 
-#open webpage
+
+# list of Movie instances
+movies = [inception, zootopia, la_la_land, whiplash, wonder_woman, sing_street]
+
+# open webpage
 fresh_tomatoes.open_movies_page(movies)
 
-
-
-
-#print(media.Movie.VALID_RATINGS )
-
-#print(media.Movie.__doc__)
-#print(media.Movie.__name__)
-#print(media.Movie.__module__)
